9b947d0fbf2c338abd704a5b16aea6d94f713ec7
igman04 <igman04@gmail.com>
2026-04-16 11:46:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "iain-vpc"
   auto_create_subnetworks = true 
 }
